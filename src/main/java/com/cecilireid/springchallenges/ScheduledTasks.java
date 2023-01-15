bdec21a797a98fa661f86028f49b9e4bde0f52fa
whathebea <bea.graciast@gmail.com>
2023-01-15 13:03:02 -0300

    Completed Second Challenge

--- src/main/java/com/cecilireid/springchallenges/ScheduledTasks.java
+++ src/main/java/com/cecilireid/springchallenges/ScheduledTasks.java
@@ -2,13 +2,20 @@ package com.cecilireid.springchallenges;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ScheduledTasks {
     private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
 
+    @Autowired
+    public CateringJobRepository cateringJobRepository;
+
+    @Scheduled(fixedRate = 10000)
     public void reportOrderStats() {
-        
+        logger.info("Number of orders: " + cateringJobRepository.count());
     }
+
 }
